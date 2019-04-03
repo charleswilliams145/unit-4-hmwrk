da6c3c09f44383ef1282725db551b490af891562
Charles Williams <charleswilliams145@gmail.com>
2019-04-03 00:27:39 -0400

    corrected reset function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,9 @@
 // varibles
 let gameSelection=Math.floor(Math.random()*102)+19;
-let cystal1=Math.floor(Math.random()*12)+1;
-let cystal2=Math.floor(Math.random()*12)+1;
-let cystal3=Math.floor(Math.random()*12)+1;
-let cystal4=Math.floor(Math.random()*12)+1;
+let crystal1=Math.floor(Math.random()*12)+1;
+let crystal2=Math.floor(Math.random()*12)+1;
+let crystal3=Math.floor(Math.random()*12)+1;
+let crystal4=Math.floor(Math.random()*12)+1;
 let userScore=0;
 let wins=0;
 let losses=0;
@@ -17,8 +17,37 @@ const scoreText=$("#score-text");
 //functions
 //restart game/ reset gameSelection and crystals and user score/reset html/
 //onclick add crystal value to user score and check for win or loss/
+const resetGame=()=>{
+ gameSelection=Math.floor(Math.random()*102)+19;
+ crystal1=Math.floor(Math.random()*12)+1;
+ crystal2=Math.floor(Math.random()*12)+1;
+ crystal3=Math.floor(Math.random()*12)+1;
+ crystal4=Math.floor(Math.random()*12)+1;
+ userScore=0;
+ targetText.text(gameSelection)
+ $("#crys1").attr("data-val",crystal1);
+ $("#crys2").attr("data-val",crystal2);
+ $("#crys3").attr("data-val",crystal3);
+ $("#crys4").attr("data-val",crystal4);
+scoreText.text(userScore); 
 
+}
 
+$("img").on("click",function(){
+    userScore+=parseInt($(this).attr("data-val"));
+    scoreText.text(userScore);
+    if(userScore===gameSelection){
+        wins++
+        winsText.text(wins);
+        resetGame()
+    }
+    if(userScore>gameSelection){
+    losses++
+        lossesText.text(losses);
+        resetGame()
+    }
+
+});
 
 //logic
 targetText.text(gameSelection)
